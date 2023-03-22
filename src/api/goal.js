6a83073e941b46069e169e51131adab826ce7adb
Robin Stempa <stemparobin@gmail.com>
2023-03-22 08:42:54 +0100

    feat added postGoal to api

--- src/api/goal.js
+++ src/api/goal.js
@@ -1,9 +1,10 @@
 import axios from "."
 const apiUrl = process.env.REACT_APP_API_URL
 
-
 export const getGoalsByProfileId = async (profileId) => {
-  const response = await axios.get(`${apiUrl}/api/v1/goals/profile/${profileId}`)
+  const response = await axios.get(
+    `${apiUrl}/api/v1/goals/profile/${profileId}`
+  )
   try {
     if (!response.status === 200) {
       throw new Error("goals not found")
@@ -15,7 +16,6 @@ export const getGoalsByProfileId = async (profileId) => {
   }
 }
 
-
 export const getGoalById = async (goalId) => {
   const response = await axios.get(`${apiUrl}/api/v1/goal/${goalId}`)
   try {
@@ -26,5 +26,40 @@ export const getGoalById = async (goalId) => {
     return [null, data]
   } catch (error) {
     return [error.message, []]
+  }
+}
+
+export const createGoal = async (token, goal) => {
+  try {
+    const response = await axios.post(`${apiUrl}/api/v1/goal`, {
+      headers: { Authorization: `Bearer  ${token}` },
+      title: goal.title,
+      start: goal.start,
+      end: goal.end,
+      achieved: goal.achieved,
+      profileId: goal.profileId,
+    })
+    if (!response.status === "200") {
+      throw new Error(response.error)
+    }
+    const data = response.data
+    return [null, data]
+  } catch (e) {
+    return [e.message, []]
+  }
+}
+
+export const deleteGoal = async (token, goalId) => {
+  try {
+    const response = await axios.delete(`${apiUrl}/api/v1/goal/${goalId}`, {
+      headers: { Authorization: `Bearer  ${token}` },
+    })
+    if (!response.status === "200") {
+      throw new Error(response.error)
+    }
+    //const data = response.data
+    //return [null, data]
+  } catch (e) {
+    return [e.message, []]
   }
 }
